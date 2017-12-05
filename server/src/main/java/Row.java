0bb311d75af8cbd94f1f458c33113b8acda916e4
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2017-12-05 22:36:15 +0100

    implemented methods in Row

--- server/src/main/java/Row.java
+++ server/src/main/java/Row.java
@@ -4,13 +4,19 @@ public class Row
     private int startIndex;
     private int length;
 
+    public Row(int startIndex, int length)
+    {
+        this.startIndex=startIndex;
+        this.length = length;
+    }
+
     public Field getField(int index)
     {
-        return fields[index];
+        return fields[index-startIndex];
     }
 
     public void setField(int index,Field field)
     {
-        this.fields[index] = field;
+        this.fields[index-startIndex] = field;
     }
 }
