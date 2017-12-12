862668b0f8c69cdc57936db5fe78ae6f141e308e
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2017-12-13 00:59:06 +0100

    new subclass - SubServer, which allows to handle all client-apps in the same time

--- server/src/main/java/SingleGameClient.java
+++ server/src/main/java/SingleGameClient.java
@@ -1,8 +1,11 @@
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
 
 public class SingleGameServer extends Thread
 {
     private ServerSocket serverSocket;
+    private Vector<SubServer> clietConnections = new Vector<SubServer>();
     
     public SingleGameServer (int port) throws IOException
     {
@@ -13,6 +16,40 @@ public class SingleGameServer extends Thread
     @Override
     public void run ()
     {
-        
+        try
+        {
+            while (!this.isInterrupted())
+            {
+                Socket connection = this.serverSocket.accept();
+                addConnectionToSubServer(connection);
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
+        
+    public void addConnectionToSubServer(Socket connection)
+    {
+        clietConnections.add(new SubServer(connection));
+    }
+    
+    protected class SubServer extends Thread
+    {
+
+        private Socket connection;
+
+        public SubServer(Socket connection)
+        {
+            try
+            {
+                this.connection = connection;
+            }
+            catch (IOException e)
+            {
+                System.out.println(e.getMessage());
+            }
+            start();
+        }
 } 
