86ee53d4253a1a87339c5260e1b3a59259f331cf
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2017-12-29 13:58:16 +0100

    new method to create Color from hex input

--- client/src/main/java/GUI/FrontTools.java
+++ client/src/main/java/GUI/FrontTools.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import javafx.scene.paint.Color;
+
 public class FrontTools
 {
     public static String RGBtoHEX(int red, int green, int blue)
@@ -11,5 +13,16 @@ public class FrontTools
         sb.append(Integer.toHexString(blue));
 
         return sb.toString();
+    }
+
+
+    public static Color HEXtoColor(String hex)
+    {
+        hex = hex.substring(1);
+        int red = Integer.parseInt(hex.substring(0,2),16);
+        int green = Integer.parseInt(hex.substring(2,4),16);
+        int blue = Integer.parseInt(hex.substring(4,6),16);
+
+        return Color.rgb(red,green,blue);
     }
 }
