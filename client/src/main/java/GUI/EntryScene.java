fbeab655007e1ff4de2a85a167ab3e10551f485f
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2018-01-02 12:52:39 +0100

    basic gui with static board

--- client/src/main/java/GUI/EntryScene.java
+++ client/src/main/java/GUI/EntryScene.java
@@ -1,22 +1,32 @@
 package GUI;
 
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
-public class EntryScene extends Scene
+
+
+public class EntryScene
 {
-    private Color backgroundColor;
-    private Parent layout;
-
-    public EntryScene (Parent root)
+    public static Scene getEntryScene(Color backgroundColor)
     {
-        super(root);
-        layout = root;
+        Color bgColor = backgroundColor;
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20,20,20,20));
+
+        TextField input = new TextField("erastdthf");
+        Button button =  new Button("earstdyfy");
+
+        layout.getChildren().addAll(input,button);
+        layout.setStyle("-fx-background-color: " + FrontTools.ColortoHEX(backgroundColor) +";");
+        Scene scene = new Scene(layout,300,200);
+
+        return scene;
     }
 
-    public void setBackgroundColor(String hex)
-    {
-        this.layout.setStyle("-fx-background-color: " + hex +";");
-    }
 }
--- client/src/main/java/GUI/MainWindow.java
+++ client/src/main/java/GUI/MainWindow.java
@@ -1,14 +1,22 @@
 package GUI;
 
+import Game.Board;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
+import java.util.Vector;
 
 public class MainWindow extends Application
 {
     private Stage window;
-    private Color backgroundColor = Color.rgb(255,248,220);
+    private Color backgroundColor;
 
     public static void main(String[] args)
     {
@@ -16,10 +24,55 @@ public class MainWindow extends Application
     }
     public void start (Stage primaryStage) throws Exception
     {
+        Vector<Circle> circles = new Vector<>();
+        Board board = new Board();
+        board.createPieces();
+
+
+        for(int i=1; i < board.rownum; i++)
+            for (int j=1; j < board.colnum; j++)
+            {
+                if(board.board[i][j])
+                {
+                    Circle circle = new Circle(10.0f);
+                    circle.setLayoutX(i * 50.0f);
+                    circle.setLayoutY(j * 50.0);
+
+
+                    circle.setOnMousePressed(e -> circle.setFill(Color.RED));
+                    circle.setOnMouseReleased(e -> circle.setFill(Color.BLACK));
+                    circles.add(circle);
+                }
+
+            }
+
+        backgroundColor = Color.rgb(255,248,220);
+        Color bgColor = backgroundColor;
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20,20,20,20));
+
+        javafx.scene.control.TextField input = new javafx.scene.control.TextField();
+        javafx.scene.control.Button button =  new Button("earstdyfy");
+
+        layout.getChildren().addAll(input,button);
+
+        Group g = new Group();
+
+        for(int i=0;i<circles.size();i++)
+            g.getChildren().add(circles.get(i));
+
+        layout.getChildren().add(g);
+
+        layout.setStyle("-fx-background-color: " + FrontTools.ColortoHEX(backgroundColor) +";");
+        Scene scene = new Scene(layout,300,200);
+
+
+
         window = primaryStage;
-        EntryScene scene = new EntryScene(new VBox());
-        scene.setBackgroundColor("#FFF8DC");
         window.setScene(scene);
+
+
         window.show();
     }
 }
