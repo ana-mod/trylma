8d9c9e4350fe7b06e2bc4b34b3b4977769b3ed8f
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2018-01-04 17:01:42 +0100

    empty commit

--- client/src/main/java/GUI/MainWindow.java
+++ client/src/main/java/GUI/MainWindow.java
@@ -5,19 +5,22 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-
-import java.util.Vector;
 
 public class MainWindow extends Application
 {
     private Stage window;
     private MenuBar menuBar;
+
+    private BorderPane mainLayout;
+    private VBox entryLayout;
+    private HBox lobbyLayout;
+
 
     public static void main(String[] args)
     {
@@ -28,17 +31,18 @@ public class MainWindow extends Application
     {
         window = primaryStage;
 
-        BorderPane mainLayout = new BorderPane();
+        mainLayout = new BorderPane();
 
         prepareMenuBar();
         mainLayout.setTop(menuBar);
         BorderPane.setMargin(menuBar,new Insets(0,0,20,0));
 
+        prepareEntryLayout();
+        mainLayout.setCenter(entryLayout);
+
+
         //Board Print test
-        Board board = new Board();
-        board.createPieces();
-        Group boardPrint = boardPrint(board);
-        mainLayout.setCenter(boardPrint);
+
 
         Scene scene = new Scene(mainLayout,300,200);
         //scene.getStylesheets().add("SceneStyle.css");
@@ -77,9 +81,25 @@ public class MainWindow extends Application
         Menu options, gameRules, info;
 
         options = new Menu("Opcje");
+        MenuItem display = new MenuItem("Wyświetlanie");
+        MenuItem connection = new MenuItem("Połączenie");
+        options.getItems().addAll(display,connection);
+
         gameRules = new Menu("Zasady Gry");
         info = new Menu("Info");
 
         menuBar.getMenus().addAll(options, gameRules, info);
+    }
+
+    private void prepareEntryLayout()
+    {
+        entryLayout = new VBox();
+        Label nicknameLabel = new Label("Pseudonim :");
+        TextField userInput = new TextField();
+        Button nextScene = new Button("nextScene");
+
+        entryLayout.getChildren().addAll(nicknameLabel, userInput, nextScene);
+
+
     }
 }
